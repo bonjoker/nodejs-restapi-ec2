f538bd36e3b6f9107a79a738e8517cb9b77e131f
bonjoker <phongthptlx98@gmail.com>
2024-08-23 14:20:41 +0700

    update restapi commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//CONNECT DATABASE
+const connectDB = require("./config/db");
+
+connectDB();
+
+
+// middlewares
+const errorHandler = require("./middlewares/error");
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const userRoutes = require("./routes/users");
+app.use("/api/users", userRoutes);
+
+app.use("/api/products", (req, res) => {
+    return res.status(200).json({
+        message: 'This is new feature change, a new route for products'
+    })
+});
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+app.use(morgan("common"));
+
+app.listen(5000, () => {
+    console.log("server is running...");
+});
