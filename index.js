1d2b408321146b6336900c7b7548dbe448b71dff
bonjoker <phongthptlx98@gmail.com>
2024-08-27 09:23:09 +0700

    Update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
 
 dotenv.config();
 
@@ -16,23 +18,35 @@ connectDB();
 
 // middlewares
 const errorHandler = require("./middlewares/error");
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 // Routes
-const authorRoutes = require("./routes/author");
-app.use("/api/author", authorRoutes);
+const userRoutes = require("./routes/users");
+app.use("/api/users", userRoutes);
 
-app.use("/api/book", (req, res) => {
+app.use("/api/products", (req, res) => {
     return res.status(200).json({
-        message: 'New route for products'
+        message: 'New route for product'
     })
 });
 
 app.use(bodyParser.json({ limit: "50mb" }));
+
+// middlewares
 app.use(cors());
+app.use(express.json());
+
 app.use(morgan("common"));
 
-app.listen(5000, () => {
-    console.log("server is running...");
+app.use(errorHandler);
+
+const server = app.listen(port, () =>
+    console.log(`Server started listening on ${port}`)
+);
+
+process.on("unhandledRejection", (error, promise) => {
+    console.log(`Logged Error: ${error}`);
+    server.close(() => process.exit(1));
 });
